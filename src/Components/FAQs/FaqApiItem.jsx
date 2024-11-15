f1a73bf9fe6256317cc336c83ea1f6a011a50074
Emma <emmaschonbeck7@gmail.com>
2024-11-15 11:45:56 +0100

    Completed full styling on the FAQ

--- src/Components/FAQs/FaqApiItem.jsx
+++ src/Components/FAQs/FaqApiItem.jsx
@@ -5,13 +5,16 @@ const FaqApiItem = ({ item, isOpen, onToggle }) => {
     <div className={`faq-item ${isOpen ? 'open' : ''}`} onClick={onToggle}>
         <div className='faq-content'>
             <h3 className='faq-title' onClick={onToggle}>{item.title}</h3>
-            {isOpen && (
-              <p className='faq-content-text'>{item.content}</p>
-            )}
+            <span className="arrow-down-icon">
+              <i className={`fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
+            </span>
+
         </div>
-        <span className="arrow-down-icon">
-          <i className={`fa-solid ${isOpen ? 'fa-chevron-up' : 'fa-chevron-down'}`}></i>
-        </span>
+        <div>
+          {isOpen && (
+            <p className='faq-content-text'>{item.content}</p>
+          )}
+        </div>
     </div>
   )
 }
